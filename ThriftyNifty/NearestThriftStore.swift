de0dae548c922bc09f687dbd669f42e6e82f72b0
andreaax <75586588+andreaax@users.noreply.github.com>
2022-06-30 03:33:58 -0400

    new

--- ThriftyNifty/NearestThriftStore.swift
+++ ThriftyNifty/NearestThriftStore.swift
@@ -5,26 +5,80 @@
 //  Created by Roumiana Mihaylova on 6/29/22.
 //
 
+import CoreLocation
 import MapKit
 import UIKit
 
 class NearestThriftStore: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+        let regionInMeters: Double = 10000
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            checkLocationServices()
+        }
+        
+        
+        func setupLocationManager() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        }
+        
+        
+        func centerViewOnUserLocation() {
+            if let location = locationManager.location?.coordinate {
+                let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+                mapView.setRegion(region, animated: true)
+            }
+        }
+        
+        
+        func checkLocationServices() {
+            if CLLocationManager.locationServicesEnabled() {
+                setupLocationManager()
+                checkLocationAuthorization()
+            } else {
+                // Show alert letting the user know they have to turn this on.
+            }
+        }
+        
+        
+        func checkLocationAuthorization() {
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedWhenInUse:
+                mapView.showsUserLocation = true
+                centerViewOnUserLocation()
+                locationManager.startUpdatingLocation()
+                break
+            case .denied:
+                // Show alert instructing them how to turn on permissions
+                break
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                // Show an alert letting them know what's up
+                break
+            case .authorizedAlways:
+                break
+            }
+        }
     }
-    */
 
-}
+
+    extension NearestThriftStore: CLLocationManagerDelegate {
+        
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let location = locations.last else { return }
+            let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+        
+        
+        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            checkLocationAuthorization()
+        }
+    }
